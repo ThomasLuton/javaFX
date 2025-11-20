40656c146399e5c10831c6a1a3e076f0da3c586e
ThomasLuton <luton.thomas60@gmail.com>
2025-11-20 11:46:42 +0100

    v1

---
+++ src/main/java/com/example/backlogtp/logic/entities/Category.java
@@ -0,0 +1,61 @@
+package com.example.backlogtp.logic.entities;
+
+import com.example.backlogtp.utils.AbstractDAO;
+
+public class Category extends AbstractDAO {
+
+    private String name;   // "Standard", "VIP"…
+    private double price;  // prix d'un ticket
+    private int capacity;  // nombre total de places disponibles
+
+	private Event event;
+    public Category() {}
+
+    public Category(String name, double price, int capacity) {
+        this.name = name;
+        this.price = price;
+        this.capacity = capacity;
+    }
+
+
+    public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+
+	public Event getEvent() {
+		return event;
+	}
+
+	public void setEvent(Event event) {
+		this.event = event;
+	}
+
+	@Override
+    public String toString() {
+        return "EventCategory{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", capacity=" + capacity +
+                '}';
+    }
+}
